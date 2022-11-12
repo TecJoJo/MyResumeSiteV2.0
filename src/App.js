1376f39ec68f4a9d8ebe363bf8ccda73b5d53498
TecJoJo <107677643+TecJoJo@users.noreply.github.com>
2022-11-12 23:57:20 +0200

    routers and skeleton done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 function App() {
-  return <h1>Good luck with the first big react project!</h1>;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
