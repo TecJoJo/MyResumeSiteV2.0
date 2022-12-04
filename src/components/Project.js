2e30e40e206f8709d3391173574168cc0e577017
TecJoJo <107677643+TecJoJo@users.noreply.github.com>
2022-12-05 01:07:12 +0200

    project component design and coding done

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,13 +1,32 @@
 import React from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function Project() {
+  const [isHover, setIsHover] = useState(false);
+  const wrapper = useRef(null);
+
+  useEffect(() => {
+    if (isHover) {
+      wrapper.current.classList.add("effect");
+    } else {
+      wrapper.current.classList.remove("effect");
+    }
+  }, [isHover]);
   return (
-    <section className="single-project-container">
+    <section ref={wrapper} className="single-project-container">
       <div className="single-project-image-container">
         <img
           src={require("../sources/computer-and-chair.jpg")}
           alt="computer and chair"
           className="project-image"
+          onMouseOver={() => {
+            setIsHover(true);
+          }}
+          onMouseOut={() => {
+            setIsHover(false);
+          }}
         />
       </div>
       <h1>First website finnished with vanilla javascript</h1>
