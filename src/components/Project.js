226be362c890c0452622ee6aa31eb1256a404a11
TecJoJo <107677643+TecJoJo@users.noreply.github.com>
2022-12-05 12:56:34 +0200

    reusable project component completed

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,36 +2,61 @@ import React from "react";
 import { useRef } from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-
-function Project() {
+// img:"../sources/computer-and-chair.jpg"
+// imgName: "computer and chair"
+// subtitle:"First website finnished with vanilla javascript"
+function Project(props) {
   const [isHover, setIsHover] = useState(false);
   const wrapper = useRef(null);
 
   useEffect(() => {
     if (isHover) {
       wrapper.current.classList.add("effect");
+      wrapper.current.style.backgroundColor = props.backgroundColor;
     } else {
       wrapper.current.classList.remove("effect");
+      wrapper.current.style.backgroundColor = "transparent";
     }
   }, [isHover]);
-  return (
-    <section ref={wrapper} className="single-project-container">
-      <div className="single-project-image-container">
-        <img
-          src={require("../sources/computer-and-chair.jpg")}
-          alt="computer and chair"
-          className="project-image"
-          onMouseOver={() => {
-            setIsHover(true);
-          }}
-          onMouseOut={() => {
-            setIsHover(false);
-          }}
-        />
-      </div>
-      <h1>First website finnished with vanilla javascript</h1>
-    </section>
-  );
+  if (props.imgIsLeft) {
+    return (
+      <section ref={wrapper} className="single-project-container">
+        <div className="single-project-image-container">
+          <img
+            src={props.img}
+            alt={props.imgName}
+            className="project-image"
+            onMouseOver={() => {
+              setIsHover(true);
+            }}
+            onMouseOut={() => {
+              setIsHover(false);
+            }}
+          />
+        </div>
+        <h1>{props.subtitle}</h1>
+      </section>
+    );
+  } else {
+    return (
+      <section ref={wrapper} className="single-project-container">
+        <h1>{props.subtitle}</h1>
+        <div className="single-project-image-container">
+          <img
+            src={props.img}
+            alt={props.imgName}
+            className="project-image"
+            onMouseOver={() => {
+              setIsHover(true);
+            }}
+            onMouseOut={() => {
+              setIsHover(false);
+            }}
+          />
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Project;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,17 @@
 import React from "react";
 import Project from "./Project";
 function Projects() {
-  return <Project />;
+  return (
+    <Project
+      img="/asset/computer-and-chair.jpg"
+      imgName="computer
+      and
+      chair"
+      subtitle="First website finnished with vanilla javascript"
+      backgroundColor="rgb(66,105,215)"
+      imgIsLeft={false}
+    />
+  );
 }
 
 export default Projects;
