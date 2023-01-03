905edc7b32232f16d4a17241f569925a2957e388
TecJoJo <107677643+TecJoJo@users.noreply.github.com>
2023-01-03 17:21:32 +0200

    contact's ENV VAR test on server

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,28 @@
 import React from "react";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <>
+      <div className="contact-container">
+        <form
+          className="contact-form"
+          action={`${process.env.REACT_APP_ENDPOINT}`}
+          autoComplete="on"
+          method="post"
+          target="blank"
+        >
+          <p>Send me a message</p>
+          <label htmlFor="name">Your name please?</label>
+          <input type="text" name="name" />
+          <label htmlFor="company">Your company please?</label>
+          <input type="text" name="company" />
+          <label htmlFor="message"></label>
+          <textarea name="message" id="" cols="30" rows="10"></textarea>
+          <button type="submit">submit</button>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default Contact;
