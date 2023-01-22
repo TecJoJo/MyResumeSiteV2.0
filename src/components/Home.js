1434ed78731e3d8a7bb30f3e6a94a4dd531fe264
Yao Lu <luyao2493751@gmail.com>
2023-01-22 09:20:27 +0200

    flipcard backside dimension calculating done, dubugging only initial rendering no dimension interpolation

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,41 +4,77 @@ import CV from "../sources/YaoLuCV.pdf";
 import Progressbar from "./Progressbar";
 import { useRef, useEffect } from "react";
 import moment from "moment";
-// flip related 
-import { useSpring, animated,config} from "@react-spring/web";
+
 
 function Home() {
-  //flip functionality
-  // const [flipped, set] = useState(false)
-  // const { transform, opacity } = useSpring({
-  //   opacity: flipped ? 1 : 1,
-  //   transform: `perspective(600px) rotateY(${flipped ? 180 : 0}deg)`,
-  //   config: config.molasses,
-  // })
-  //end of flip functionality
+
 
   const windowInnerHeight = window.innerHeight;
   const progressbarWrapper = useRef(null);
+  const flipcardBackside = useRef(null)
   const [dimensions, setDimensions] = useState({});
   const [time, setTime] = useState(
     moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
   );
+ 
+  
+  //initial track of the dimension useState after the initial render
 
+  useEffect(()=>{
+    //calculateOffset()
+    try {
+      console.log("this is the initial rendering");
+      console.log(progressbarWrapper.current.getBoundingClientRect());
+      const {top,height,width} =
+        progressbarWrapper.current.getBoundingClientRect();
+     
+      const togglePoint = windowInnerHeight - height;
+
+      setDimensions({ top, height, togglePoint, width});
+      console.log(flipcardBackside.current.getBoundingClientRect());
+      flipcardBackside.current.style.width = `${dimensions.width}px`;
+      flipcardBackside.current.style.height = `${dimensions.height}px`
+      console.log(flipcardBackside.current.getBoundingClientRect());
+      
+      console.log("dimentions setted");
+      
+      // console.log(top, height, windowInnerHeight, togglePoint);
+    } catch (TypeError) {}
+  },[])
+
+  //callback, calculate the demensions of the flipcard front page.
   const calculateOffset = () => {
     try {
-      const { top, height } =
+      console.log(progressbarWrapper.current.getBoundingClientRect());
+      const {top,height,width} =
         progressbarWrapper.current.getBoundingClientRect();
+     
       const togglePoint = windowInnerHeight - height;
-      setDimensions({ top, height, togglePoint });
+
+      setDimensions({ top, height, togglePoint, width});
+      console.log(flipcardBackside.current);
+      flipcardBackside.current.style.width = `${dimensions.width}px`;
+      flipcardBackside.current.style.height = `${dimensions.height}px`
+      console.log("dimentions setted");
+      
       // console.log(top, height, windowInnerHeight, togglePoint);
     } catch (TypeError) {}
   };
   useEffect(() => {
+    console.log("scroll even litsener is added");
+    
+
     window.addEventListener("scroll", calculateOffset);
-    // return () => {
-    //   window.removeEventListener("scroll", calculateOffset);
-    // };
-  }, []);
+    console.log("resize even litsener is added");
+    window.addEventListener("resize", calculateOffset);
+    return () => {
+      console.log("scroll even litsener is removed");
+      
+      window.removeEventListener("scroll", calculateOffset);
+      console.log("resize even litsener is removed");
+      window.removeEventListener("resize", calculateOffset)
+    };
+  },[dimensions]);
 
   useEffect(() => {
     const timeOut = setTimeout(() => {
@@ -49,6 +85,10 @@ function Home() {
     };
   }, [time]);
 
+ 
+
+
+  // console.log("page render");
   return (
     <>
       <section className="hero-head-container">
@@ -86,10 +126,10 @@ function Home() {
         </button>
       </section>
       <div className="blank-space"></div>
-      <section className="home-card" >{/*onClick={()=>{set(flipped?!flipped:!flipped)}}*/}
+      <section className="flip-card-container" >
 
       
-      <div ref={progressbarWrapper}  className="progressbar-wrapper">{/*style={{opacity,transform}}*/ }
+      <div ref={progressbarWrapper}  className="progressbar-wrapper">
         
         <Progressbar
           togglePoint={dimensions.togglePoint}
@@ -142,6 +182,9 @@ function Home() {
           bgcolor2="rgb(201, 125, 245,0.8)"
         />
         <h1 className="skillTitle">Programming skills</h1>
+      </div>
+      <div className="flipcard-backside" ref={flipcardBackside}>
+
       </div>
       </section>
     </>
