e088ff6616f343036427cb21b706b3a7a29f19cf
Yao Laptop <luyao2493751@gmail.com>
2024-01-05 00:20:30 +0200

    messages update

--- src/components/home/messages.js
+++ src/components/home/messages.js
@@ -1,7 +1,7 @@
 const welcome = "Hi, there!  welcome to my website"
 const headline = "I am Yao, I am working as a software development trainee for Beamex located in Finland."
 const secondary = " I am also an IT student from HAMK University of Applied Sciences"
-const companiesWorked = "Companies and projects I have been working along with"
+const companiesWorked = "Companies and projects I have been working as a software trainee"
 const techToolUsed = "Technical tools I have been used"
 const info1 = "The project is still uder development, portfolio project v1.0 entry"
 const info2 = "contact page form is currently not working, please dont fill form there"
